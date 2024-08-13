922cf15d7008dd6e643923a47bd25814491488ef
PrAdEePkUmAr07102 <pradeep07spk@gmail.com>
2024-08-13 15:45:08 +0530

    route structure

--- main.js
+++ main.js
@@ -9,6 +9,20 @@ app.get("/",(req,res)=>{
 });
 
 
+//CRUD FUNCTIONALITY OF MOVIES
+
+// R-FOR READING A MOVIES
+app.get("/movies",()=>{});
+
+//C-FOR CREATING AA MOVIES
+app.post("/movies",()=>{});
+
+//U-FOR UPDATE A MOVIE
+app.put("/movies/:id",()=>{});
+
+//D-FOR DELETE A MOVIE
+app.delete("/movies/:id",()=>{});
+
 app.listen(PORT, ()=>{
     console.log(`The Server Is Running at http://localhost:${PORT}`);
     
