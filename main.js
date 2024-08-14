cb1440d0158f9bcb8559007652cf692b9066e585
PrAdEePkUmAr07102 <pradeep07spk@gmail.com>
2024-08-14 14:58:12 +0530

    CONNECTED WITH DB

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 import express from "express";
 import movieRoutes from "./routes/movies.routes.js"
+import connectDB from "./lib/db.js";
 
 
 const app = express();
 const PORT = 6969;
 
+
+//CONNECT DB
+connectDB();
 
 app.get("/",(req,res)=>{
     res.json({ msg: "HELLO STUDENTS!"});
