b99ec8a0e740b9348af92aa0462b2fa3c42c0714
PrAdEePkUmAr07102 <pradeep07spk@gmail.com>
2024-08-14 12:20:30 +0530

    add controller files

---
+++ controllers/movies.controller.js
@@ -0,0 +1,19 @@
+export const MovieIndex = (req,res)=>{
+    res.send("Get all movies");
+}
+
+
+export const MovieCreate = (req,res)=>{
+    res.send("Create all Movies");
+};
+
+
+export const MovieUpdate = (req,res)=>{
+    res.send("Update all movies");
+}
+
+
+export const MovieDelete = (req,res)=>{
+    res.send("Delete all movie")
+}
+
